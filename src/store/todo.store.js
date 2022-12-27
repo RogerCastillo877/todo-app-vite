8b8b1f46495ecfafba49fdac3780a4859e32578f
RogerCastillo877 <roger.castillo877@gmail.com>
2022-12-27 18:03:51 -0500

    methods used by the store

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -20,6 +20,45 @@ const initialStore = () => {
   console.log('Initial Store');
 }
 
+const loadStore = () => {
+  throw new Error('Not implemented')
+}
+
+/**
+ * 
+ * @param { String } description 
+ */
+const addTodo = (description) => {
+  throw new Error('Not implemented')
+}
+
+const toggleTodo = (todoId) => {
+  throw new Error('Not implemented')
+}
+
+const deleteTodo = (todoId) => {
+  throw new Error('Not implemented')
+}
+
+const deleteCompleted = (todoId) => {
+  throw new Error('Not implemented')
+}
+
+const setFilter = (newFilter = Filters.All) => {
+  throw new Error('Not implemented')
+}
+
+const getCurrentFilter = () => {
+  throw new Error('Not implemented')
+}
+
 export default {
+  addTodo,
+  deleteCompleted,
+  deleteTodo,
+  getCurrentFilter,
   initialStore,
+  loadStore,
+  setFilter,
+  toggleTodo,
 }
