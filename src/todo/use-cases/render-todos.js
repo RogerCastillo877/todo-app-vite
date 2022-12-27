f582cf6778f4b689f8d3a94c51503026aee05a28
RogerCastillo877 <roger.castillo877@gmail.com>
2022-12-27 18:56:27 -0500

    validations and optimizations

--- src/todo/use-cases/render-todos.js
+++ src/todo/use-cases/render-todos.js
@@ -1,6 +1,7 @@
 import { Todo } from "../models/todo.model";
 import { createTodoHTML } from "./";
 
+let element;
 /**
  * 
  * @param { String } elementId 
@@ -8,7 +9,13 @@ import { createTodoHTML } from "./";
  */
 export const renderTodos = (elementId, todos = []) => {
 
-  const element = document.querySelector(elementId);
+  if (!element)
+    element = document.querySelector(elementId);
+
+  if (!element) throw new Error(`Element ${elementId} not found`);
+
+  element.innerHTML = '';
+
   todos.forEach(todo => {
     element.append(createTodoHTML(todo));
   });
