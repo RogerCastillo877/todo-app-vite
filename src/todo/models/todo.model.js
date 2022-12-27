52c65094a32c9a62e972d9798cf49680557d085a
RogerCastillo877 <roger.castillo877@gmail.com>
2022-12-27 17:45:40 -0500

    Generate unique ID with UUID

--- src/todo/models/todo.model.js
+++ src/todo/models/todo.model.js
@@ -1,10 +1,11 @@
+import { v4 as uuid } from 'uuid';
 
 export class Todo {
   /**
    * @param { string } description
   */
   constructor(description) {
-    this.id = 1;
+    this.id = uuid();
     this.description = description;
     this.done = false;
     this.createAt = new Date();
