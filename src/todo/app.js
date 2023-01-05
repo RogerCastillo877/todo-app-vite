2f6f41897dca06d24d9ae1737a27446958a8b22c
RogerCastillo877 <roger.castillo877@gmail.com>
2023-01-05 17:59:45 -0500

    delete a todo

--- src/todo/app.js
+++ src/todo/app.js
@@ -35,9 +35,18 @@ export const App = (elementId) => {
     displayTodos();
     event.target.value = '';
   });
+
   todoListUL.addEventListener('click', (event) => {
     const element = event.target.closest('[data-id]');
     todoStore.toggleTodo(element.getAttribute('data-id'));
     displayTodos();
-  })
+  });
+
+  todoListUL.addEventListener('click', (event) => {
+    const isDestroyElement = event.target.className === 'destroy';
+    const element = event.target.closest('[data-id]');
+    if (element && !isDestroyElement) return;
+    todoStore.deleteTodo(element.getAttribute('data-id'));
+    displayTodos();
+  });
 };
